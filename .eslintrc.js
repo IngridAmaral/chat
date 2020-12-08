05763c9e48e9bdba2e7fa23f5a6ef73b3a35ce3d
Ingrid Amaral <ingridamaralrj@gmail.com>
2020-12-08 10:58:44 -0300

    configures eslint

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 11,
+    sourceType: 'module'
+  },
+  extends: ['react-app', 'airbnb', 'plugin:jsx-a11y/recommended', 'prettier', 'prettier/react'],
+  plugins: ['jsx-a11y', 'prettier'],
+  rules: {
+    semi: 0,
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }]
+  }
+};
