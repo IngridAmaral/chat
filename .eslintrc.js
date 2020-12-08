4040d5ca2ad3a1539adce45d72e04f6998d7e836
Ingrid Amaral <ingridamaralrj@gmail.com>
2020-12-08 12:11:15 -0300

    runs prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,13 @@ module.exports = {
     ecmaVersion: 11,
     sourceType: 'module'
   },
-  extends: ['react-app', 'airbnb', 'plugin:jsx-a11y/recommended', 'prettier', 'prettier/react'],
+  extends: [
+    'react-app',
+    'airbnb',
+    'plugin:jsx-a11y/recommended',
+    'prettier',
+    'prettier/react'
+  ],
   plugins: ['jsx-a11y', 'prettier'],
   rules: {
     semi: 0,
