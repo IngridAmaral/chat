91ba200b698fc4f03d1dd16f4f3f56f5123cf0f8
Ingrid Amaral <ingridamaralrj@gmail.com>
2020-12-08 13:04:35 -0300

    creates the Input for new messages

---
+++ src/components/form/input-message/InputMessage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './InputMessage.scss';
+
+const InputMessage = () => (
+  <input type="text" className="input-message" placeholder="Message" />
+);
+
+export default InputMessage;
