d18afe460a109c97f61bfa402de291a028215134
Ingrid Amaral <ingridamaralrj@gmail.com>
2020-12-18 17:28:40 +0100

    adds textarea autosize

--- src/components/form/input-message/InputMessage.jsx
+++ src/components/form/input-message/InputMessage.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import TextareaAutosize from 'react-textarea-autosize';
 import './InputMessage.scss';
 
-const InputMessage = ({ handleInput }) => (
-  <input
-    type="text"
+const InputMessage = ({ handleInput, newMessage }) => (
+  <TextareaAutosize
+    maxRows={3}
+    minRows={1}
     className="input-message"
     onChange={handleInput}
-    placeholder="Message"
+    placeholder="Type your message"
+    value={newMessage}
   />
 );
 
 InputMessage.propTypes = {
-  handleInput: PropTypes.func.isRequired
+  handleInput: PropTypes.func.isRequired,
+  newMessage: PropTypes.string.isRequired
 };
 
 export default InputMessage;
