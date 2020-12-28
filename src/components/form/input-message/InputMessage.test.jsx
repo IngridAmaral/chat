b2a083d8c0e0c7220e55ab9ef5d99589749f1908
Ingrid Amaral <ingridamaralrj@gmail.com>
2020-12-28 11:47:40 +0100

    updates input tests

--- src/components/form/input-message/InputMessage.test.jsx
+++ src/components/form/input-message/InputMessage.test.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import TextareaAutosize from 'react-textarea-autosize';
 import InputMessage from './InputMessage';
 
+const defaultProps = {
+  handleInput() {},
+  newMessage: ''
+};
+
 it('renders component', () => {
-  shallow(<InputMessage />);
+  shallow(<InputMessage {...defaultProps} />);
+});
+
+it('renders the textareaAutosize', () => {
+  const wrapper = shallow(<InputMessage {...defaultProps} />);
+
+  expect(wrapper.find(TextareaAutosize).exists()).toBe(true);
+});
+
+it('should trigger onChange function when typing', () => {
+  const handleInput = jest.fn();
+  const event = { target: { value: 'test' } };
+  const wrapper = shallow(
+    <InputMessage {...defaultProps} handleInput={handleInput} />
+  );
+
+  wrapper.find(TextareaAutosize).simulate('change', event);
+  expect(handleInput).toHaveBeenCalledWith(event);
 });
